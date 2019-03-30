2e7246febe8691b478d86ad0363955afc6bf5acf
Chris Shepherd <admin@thesheps.dev>
2019-03-30 11:12:53 +0000

    Updates

---
+++ precache-manifest.94fa11bbee33ecc0922a54db138f664f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b697e3ec0958b0d66ba1",
+    "url": "/mobster-time/js/app.8697fd2c.js"
+  },
+  {
+    "revision": "737591ab5c8cb065d441",
+    "url": "/mobster-time/css/chunk-vendors.47209020.css"
+  },
+  {
+    "revision": "737591ab5c8cb065d441",
+    "url": "/mobster-time/js/chunk-vendors.e5e3b868.js"
+  },
+  {
+    "revision": "da2b82b01358d0334ffcaad1e9dd0991",
+    "url": "/mobster-time/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mobster-time/robots.txt"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mobster-time/precache-manifest.a58ac1c6c1f694991216045e1fbd94a3.js"
+  "/mobster-time/precache-manifest.94fa11bbee33ecc0922a54db138f664f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "mobster-time"});
