b025de6f1131f49941ae6b39199cad204e6419bb
Chris Shepherd <thesheps@gmail.com>
2019-03-30 09:50:11 +0000

    Reapplies PWA

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from "vue";
-import './plugins/vuetify'
+import "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
