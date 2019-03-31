1718313f1817b54300760ef460d206817729ffc7
Chris Shepherd <thesheps@gmail.com>
2019-03-31 13:41:09 +0100

    Adjustment to title

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
         <span>Mobster</span>
-        <span class="font-weight-light">Time</span>
+        <span class="font-weight-light">The collaboration timer</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
 
